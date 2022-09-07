ba04361489a2ca0ed222bc97645ebc8516e3b2bc
Aliaksei Haretski <haretski.aliaksei@gmail.com>
2022-09-07 22:07:33 +0200

    add lesson01 Task03

--- lesson01/Task03.java
+++ lesson01/Task03.java
@@ -2,6 +2,6 @@ package lesson01;
 
 public class Task03 {
     public static void main(String[] args) {
-        System.out.println("Java: version 11.");
+        System.out.println("Java: version 11." );
     }
 }
