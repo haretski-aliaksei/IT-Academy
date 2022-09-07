41dd97f15e0792f7c30cef68de1e61330dbbb44a
Aliaksei Haretski <haretski.aliaksei@gmail.com>
2022-09-07 21:41:33 +0200

    add lesson01 Task4

---
+++ lesson01/Task4.java
@@ -0,0 +1,8 @@
+package lesson01;
+
+public class Task4 {
+    public static void main(String[] args) {
+        System.out.println("Java: version 11.\n" +
+                "JVM: HotSpot.");
+    }
+}
