d9201161cc34676570372cad049c4879cdac35e4
Aliaksei Haretski <haretski.aliaksei@gmail.com>
2022-09-07 22:24:53 +0200

    add lesson02 Task03

---
+++ lesson02/Task03.java
@@ -0,0 +1,9 @@
+package lesson02;
+
+public class Task03 {
+    public static void main(String[] args) {
+        float r = 5.5F;
+        System.out.println("Площадь круга равна " + Math.PI * Math.pow(r, 2));
+        System.out.println("Длина окружности круга равна " + 2 * Math.PI * r);
+    }
+}
