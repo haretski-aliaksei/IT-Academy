08c55e4fae03a447f6ec660291cd8bb657380515
aliaksei_h <haretski.aliaksei@gmail.com>
2022-09-07 21:46:04 +0200

    Delete Task4.java

--- lesson01/Task4.java
+++
@@ -1,8 +0,0 @@
-package lesson01;
-
-public class Task4 {
-    public static void main(String[] args) {
-        System.out.println("Java: version 11.\n" +
-                "JVM: HotSpot.");
-    }
-}
