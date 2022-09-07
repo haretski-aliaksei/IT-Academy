4817b2560fbc696fdc13048664f1a636b7caf3ef
Aliaksei Haretski <haretski.aliaksei@gmail.com>
2022-09-07 22:11:51 +0200

    add lesson02 Task01

--- lesson02/Task01.java
+++ lesson02/Task01.java
@@ -4,8 +4,8 @@ public class Task01 {
     public static void main(String[] args) {
         int x = 10;
         int y = 3;
-        System.out.println("результат деления \"x\" на \"y\" равен " + x / y);
-        System.out.println("остаток от деления \"x\" на \"y\" равен " + x % y);
+        System.out.println("Результат деления \"x\" на \"y\" равен " + x / y);
+        System.out.println("Остаток от деления \"x\" на \"y\" равен " + x % y);
     }
 
 }
