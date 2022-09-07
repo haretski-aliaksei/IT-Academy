3495188bb961d296f8b507aa9e13626b25783e6b
Aliaksei Haretski <haretski.aliaksei@gmail.com>
2022-09-07 22:13:03 +0200

    add lesson02 Task02

--- lesson02/Task02.java
+++ lesson02/Task02.java
@@ -4,7 +4,7 @@ public class Task02 {
     public static void main(String[] args) {
         float w = 10.5F;
         float y = 5F;
-        System.out.println("периметр прямоугольника равен " + (w + y) * 2);
-        System.out.println("площадь прямоугольника равен " + w * y);
+        System.out.println("Периметр прямоугольника равен " + (w + y) * 2);
+        System.out.println("Площадь прямоугольника равен " + w * y);
     }
 }
