59fccd63d9fa85f000c563f6c8ed9df81fbe8752
aliaksei_h <haretski.aliaksei@gmail.com>
2022-09-14 20:27:36 +0200

    Delete lesson04 directory

--- lesson04/Task01.java
+++
@@ -1,9 +0,0 @@
-package lesson04;
-
-public class Task01 {
-    public static void main(String[] args) {
-        for (int i = 0; i < 11; i++) {
-            System.out.println(i);
-        }
-    }
-}
--- lesson04/Task02.java
+++
@@ -1,9 +0,0 @@
-package lesson04;
-
-public class Task02 {
-    public static void main(String[] args) {
-        for (int i = 10; i >= 0; i--) {
-            System.out.println(i);
-        }
-    }
-}
